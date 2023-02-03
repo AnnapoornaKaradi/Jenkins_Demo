e0d1c02a2c40550320d7889993e5304ca4129535
Annu <annapoornakaradi96@gmail.com>
2023-02-03 22:22:31 +0530

    Added the new repo

---
+++ src/test/java/testCasePackage/TestClass.java
@@ -0,0 +1,13 @@
+package testCasePackage;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestClass {
+
+	@Test
+	public void test() {
+		Assert.assertEquals("welcome to login page",new String("welcome to login page"));
+	}
+
+}
